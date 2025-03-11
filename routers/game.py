97fef80dedd853826a40c310de4e8387b2305f82
Hanjoo Kim <hanjoo0211@gmail.com>
2025-03-11 17:22:58 +0900

    Feat: Add game round system

--- routers/game.py
+++ routers/game.py
@@ -21,8 +21,32 @@ async def websocket_endpoint(websocket: WebSocket):
     
     try:
         while True:
+            # 클라이언트로부터 메시지 수신
             data = await websocket.receive_json()
+
+            if game_manager.game_started:
+                # 이미지 수신
+                if data["type"] == "image":
+                    player_index = game_manager.players.index(websocket)
+                    game_manager.images[player_index].append(data["data"])
+                    
+                    # 모든 플레이어가 이미지를 제출했는지 확인
+                    if game_manager.check_image(game_manager.game_round):
+                        # 각 플레이어들에게 다음 이미지 전송
+                        for player in game_manager.players:
+                            next_image = game_manager.next_image(player)
+                            await player.send_json(next_image)
+                        # 라운드 넘기거나 게임 종료
+                        if game_manager.game_round < game_manager.max_players:
+                            game_manager.next_round()
+                        else: # 게임 종료 시 전체 사진 전송
+                            await broadcast({"type": "game_end", "images": game_manager.images}, game_manager.players)
+                            # game_manager.game_started = False
+                            # game_manager.game_round = 0
+                            # game_manager.images = [[], [], [], []]
+
             await broadcast(data, game_manager.players)
     except WebSocketDisconnect:
+        # 클라이언트 연결 종료 시 플레이어 제거
         game_manager.remove_player(websocket)
         await broadcast({"players": len(game_manager.players), "game_started": game_manager.game_started}, game_manager.players)
--- services/game_manager.py
+++ services/game_manager.py
@@ -3,7 +3,10 @@ class GameManager:
         self.players = []  # 참가자 목록
         self.max_players = 4  # 최대 참가자 수
         self.game_started = False  # 게임 시작 여부
+        self.game_round = 0 # 게임 라운드
         self.words = ["임승섭", "채지헌", "최건호", "김한주"]  # 단어 목록
+        self.images = [[], [], [], []] # 이미지 목록 (플레이어 순서로 index)
+
 
     def add_player(self, websocket):
         """플레이어 추가"""
@@ -13,6 +16,7 @@ class GameManager:
         # 4명이 되면 게임 시작
         if len(self.players) == self.max_players:
             self.game_started = True
+            self.next_round()
             return True  # 게임이 시작되었음을 반환
         return False  # 아직 시작 전
 
@@ -30,9 +34,31 @@ class GameManager:
         return {"players": len(self.players), "game_started": self.game_started}
 
     def get_word(self, websocket):
-        """단어 전송"""
+        """플레이어별 단어 반환"""
         if self.game_started and websocket in self.players:
             player_index = self.players.index(websocket)
             word = self.words[player_index]
             return {"word": word}
         return {"word": ""}
+
+    def check_image(self, round):
+        """라운드 별로 모든 플레이어가 이미지를 제출했는지 확인"""
+        if self.game_round == round:
+            for i in range(self.max_players):
+                if len(self.images[i]) < round:
+                    return False
+            return True
+        return False
+
+    def next_round(self):
+        """라운드 넘기기"""
+        self.game_round += 1
+        print("Next Round: ", self.game_round)
+        return self.game_round
+    
+    def next_image(self, websocket):
+        """다음 이미지 전송"""
+        player_index = self.players.index(websocket)
+        round = self.game_round
+        next_image_index = (player_index + round) % self.max_players
+        return {"image": self.images[next_image_index][round - 1]}
