9b902355fb639350b9560fcd530ec47d53d42a12
Hanjoo Kim <hanjoo0211@gmail.com>
2025-03-18 09:47:52 +0900

    Chore: Change ping interval

--- start_server.sh
+++ start_server.sh
@@ -5,4 +5,4 @@ source .venv/bin/activate
 
 # Uvicorn 서버 실행
 # uvicorn main:app --host 0.0.0.0 --port 8000 --reload
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload --ws-ping-interval 30 --ws-ping-timeout 1200
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload --ws-ping-interval 10 --ws-ping-timeout 1200
